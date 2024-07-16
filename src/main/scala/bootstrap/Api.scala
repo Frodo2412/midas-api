89a007f0d60f002031220c9aa295e9db53d03735
Frodo2412 <brunolemus553@gmail.com>
2024-07-16 18:10:33 +0200

    feat: swagger docs

--- src/main/scala/bootstrap/Api.scala
+++ src/main/scala/bootstrap/Api.scala
@@ -7,17 +7,25 @@ import scribe.Scribe
 import sttp.tapir.*
 import sttp.tapir.server.ServerEndpoint
 import sttp.tapir.server.http4s.Http4sServerInterpreter
+import sttp.tapir.swagger.bundle.SwaggerInterpreter
 
 class Api[F[_]: Async: Scribe]:
 
-  private val healthEndpoint =
+  private val healthEndpoint: ServerEndpoint[Any, F] =
     endpoint.get
       .in("health")
       .out(stringJsonBody)
       .serverLogicSuccessPure[F](_ => "Still alive...")
 
+  private val docs = SwaggerInterpreter()
+    .fromServerEndpoints[F](
+      List(healthEndpoint),
+      "Midas API",
+      "0.1.1"
+    )
+
   def routes: HttpRoutes[F] =
     Http4sServerInterpreter[F]()
-      .toRoutes(healthEndpoint)
+      .toRoutes(healthEndpoint :: docs)
 
 end Api
