7bfff47f4e5e99e707f505640d47a424e78179da
Frodo2412 <brunolemus553@gmail.com>
2024-07-17 16:47:55 +0200

    feat: handle duplicate email

--- domains/security/src/main/scala/interpreters/SkunkUsersInterpreter.scala
+++ domains/security/src/main/scala/interpreters/SkunkUsersInterpreter.scala
@@ -7,11 +7,13 @@ import models.User
 import cats.effect.{MonadCancelThrow, Resource}
 import cats.syntax.all.*
 import com.principate.midas.lib.newtypes.GenUUID
+import com.principate.midas.security.algebras.Users.CreateUserError.EmailAlreadyInUse
+import org.postgresql.util.PSQLException
 import scribe.Scribe
 import skunk.codec.all.uuid
 import skunk.codec.text.*
 import skunk.implicits.sql
-import skunk.{Command, Query, Session}
+import skunk.{Command, Query, Session, SqlState}
 
 import java.util.UUID
 
@@ -29,13 +31,18 @@ final class SkunkUsersInterpreter[F[_]: MonadCancelThrow: GenUUID: Scribe](
     Scribe[F].trace(
       s"Creating user: { email: $email, firstName: $firstName, lastName: $lastName"
     ) *>
-      sessionPool.use: session =>
-        for
-          cmd <- session.prepare(CreateUserCommand)
-          _   <- Scribe[F].trace(cmd.toString)
-          id  <- GenUUID[F].make[UUID]
-          _   <- cmd.execute(id, email, firstName, lastName, passwordHash)
-        yield Right(User(id, email, firstName, lastName))
+      sessionPool
+        .use: session =>
+          for
+            cmd <- session.prepare(CreateUserCommand)
+            _   <- Scribe[F].trace(cmd.toString)
+            id  <- GenUUID[F].make[UUID]
+            _   <- cmd.execute(id, email, firstName, lastName, passwordHash)
+          yield Right(User(id, email, firstName, lastName))
+        .recoverWith:
+          case SqlState.UniqueViolation(ex)
+              if ex.columnName.contains("email") =>
+            MonadCancelThrow[F].pure(Left(EmailAlreadyInUse(email)))
 
   override def find(userId: UUID): F[Option[User]] =
     Scribe[F].trace(s"Fetching user with id: $userId") *>
